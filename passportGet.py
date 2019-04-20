2ec8225eb13ca070441cb5a49c89d2efccf23483
Shaohua <cell.fantasy@qq.com>
2019-04-20 11:51:27 +0800

    Update passportGet.py

--- passportGet.py
+++ passportGet.py
@@ -7,51 +7,131 @@ import os
 import re
 import xlwt
 
-def listPP():
+def listPic():
 	for root, dirs, files in os.walk('.\\passport\\'):  #root:当前目录路径; dirs:当前路径下所有子目录; files:当前路径下所有非目录子文件
-		print(files)
+		#print(files)
 		return files
 
-def getInfo():
-	lst_pp=listPP()
-	PPdata=[['照片名','护照姓','护照名','护照号码']]
-	for pic_name in lst_pp:
-		try:
-			result=os.popen('tesseract .\\passport\\%s rst -l eng -psm 1'%pic_name)#-psm 7
-			res = result.read()  
-			for line in res.splitlines():  
-				print (line+'\n') 
-
-			with open('rst.txt','r',encoding='UTF-8') as txt:
-				txt=txt.read()
-				Surname=re.findall(r'P<(.*?)<<',txt)[0]
-				if Surname == 'IND':
-					Surname=''
-				givenName=re.findall(r'<<(.*?)<<<<<<<<<<<',txt)[0]
-				#if '<' in givenName:
-				givenName=givenName.replace('<',' ')	
-				PPno=re.findall(r'<<<<<<<\n(.*?)<',txt)[0]
-				PPdata.append([pic_name,Surname,givenName,PPno])
-				#print ('Surname is: %s\nGiven Name is: %s\nPassport Number is: %s'%(Surname,givenName,PPno))
-				print (PPdata)
-		except Exception as e:
-			print (pic_name,e)
-			continue
+def vsGet(pic_name,txt,vsdata):
+	try:
+		Surname=re.findall(r'R<CHN(.*?)<<',txt)[0].replace('\'','').replace('’','').replace(' ','')
+		givenName=re.findall(r'R<CHN.*?<<(.*?)<<<',txt)[0].replace(' ','').replace('<',' ').replace('\'','').replace('’','')
+		vs_num=re.findall(r'\n(.*?)<.*?<<',txt)
+		#print (vs_num)
+		vs_num=[i for i in vs_num if i !='']
+		vs_num=vs_num[-1].replace('\'','').replace('’','').replace(' ','').replace('z','2').replace('Z','2').replace('D','0').replace('O','0').replace('o','0')
+		mix=re.findall(r'\n.*?<(.*?)<<',txt)[1].replace('\'','').replace('’','').replace(' ','').replace('z','2').replace('Z','2').replace('D','0').replace('O','0').replace('o','0')
+		bdate=mix[4:10]
+		if int(bdate[0])>5:
+			bdate='19'+bdate
+		else:
+			badate='20'+bdate
+		sex=mix[11:12]
+		if sex=='H' or sex=='M':
+			sex='男'
+		else:
+			sex='女'
+		expire='20'+mix[12:18]
+		vsdata.append([pic_name,Surname,givenName,vs_num,bdate,sex,expire])
+		#print (vsdata)
+	except Exception as e:
+		print (pic_name,e)
+		vsdata.append([pic_name])
+	return vsdata
+		
+def ppGet(pic_name,txt,PPdata):
+	try:
+		Surname=re.findall(r'P<(.*?)<<',txt)[0].replace('\'','').replace('’','').replace(' ','')
+		#print (Surname)
+		if len(Surname)==3:
+			Nation=Surname
+			Surname=''
+		else:
+			Nation=Surname[0:3]
+			Surname=Surname[3:]
+		givenName=re.findall(r'<<(.*?)<<<<<<<<<<<',txt)[0].replace(' ','').replace('<',' ').replace('\'','').replace('’','')
+		PPno=re.findall(r'<<<<<<<\n(.*?)<',txt)[0].replace('\'','').replace('’','').replace(' ','')
+		mix=re.findall(r'<(.*?)<<<<',txt)
+		mix=[i for i in mix if i !='']
+		mix=mix[-1].replace('\'','').replace('’','').replace(' ','').replace('z','2').replace('Z','2').replace('D','0').replace('O','0').replace('o','0')
+		#print (mix)
+		bdate=mix[4:10]
+		if int(bdate[0])>5:
+			bdate='19'+bdate
+		else:
+			badate='20'+bdate
+		sex=mix[11:12]
+		if sex=='H' or sex=='M':
+			sex='男'
+		else:
+			sex='女'
+		expire='20'+mix[12:18]
+		PPdata.append([pic_name,Surname,givenName,Nation,PPno,bdate,sex,expire])
+		#print (PPdata)
+	except Exception as e:
+		print (pic_name,e)
+		PPdata.append([pic_name])
+		#continue
 	return PPdata
 
-def wtData():
-	PPdata=getInfo()
+def wtInfo(PPdata,vsdata):
 	f = xlwt.Workbook()
-	sheet1 = f.add_sheet(u'sheet1',cell_overwrite_ok=True) #创建sheet 
+	
+	sheet1 = f.add_sheet(u'pp',cell_overwrite_ok=True) #创建sheet 
 	#将数据写入第 i 行，第 j 列
 	i = 0
 	for data in PPdata:
 		for j in range(len(data)):
 			sheet1.write(i,j,data[j])
-		i = i + 1       
+		i = i + 1
+		
+	sheet2 = f.add_sheet(u'vs',cell_overwrite_ok=True) #创建sheet 
+	#将数据写入第 i 行，第 j 列
+	i = 0
+	for data in vsdata:
+		for j in range(len(data)):
+			sheet2.write(i,j,data[j])
+		i = i + 1
+		
 	f.save('passportInfo.xls') #保存文件
+	
+def split():
+	lstPic=listPic()
+	vsdata=[['照片名','护照姓','护照名','居留许可号','生日','性别','许可有效期至']]
+	PPdata=[['照片名','护照姓','护照名','国籍','护照号码','生日','性别','护照有效期至']]
+	for pic_name in lstPic:
+		print (pic_name)
+		result=os.popen('tesseract .\\passport\\%s rst -l eng --psm 1'%pic_name)#-psm 7
+		res = result.read()  
+		for line in res.splitlines():  
+			print (line+'\n') 
+		with open('rst.txt','r',encoding='UTF-8') as txt:
+			txt=txt.read()
+			if 'R<CHN' in txt:
+				vsGet(pic_name,txt,vsdata)
+			else:
+				ppGet(pic_name,txt,PPdata)
+	wtInfo(PPdata,vsdata)
 
-wtData()
+def pil():
+	lstPic=listPic()
+	for pic_name in lstPic:
+		path='.\\passport\\'+pic_name
+		image=Image.open(path)
+		image=image.convert('L')
+		threshold=130
+		table=[]
+		for i in range(256):
+			if i<threshold:
+				table.append(0)
+			else:
+				table.append(1)
+		image=image.point(table,'1')
+		#image.show()
+		image.save('.\\test\\%s'%pic_name,quality=95)
+		print ('new image saved')
+#pil()
+split()
 
 
 '''
